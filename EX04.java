f83cbaffdc5a9741bbb426dc355af12eb1373bb1
Marcos Lucas de Souza Machado <118454658+F1reZap@users.noreply.github.com>
2025-09-22 09:59:34 -0300

    Add files via upload

---
+++ EX04.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class EX04 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Random random = new Random();
+
+        int a, b, c;
+
+        System.out.print("Digite um numero: ");
+        a = teclado.nextInt();
+        System.out.println(".");
+        System.out.print("Digite um numero: ");
+        b = teclado.nextInt();
+        System.out.println(".");
+        System.out.print("Digite um numero: ");
+        c = teclado.nextInt();
+
+
+        System.out.println("====================");
+        con(a, b, c);
+    }
+
+    private static void con(int a, int b, int c) {
+
+        double delta1, delta2, delta3;
+
+        delta1 = Math.pow(b, 2);
+        delta2 = 4*a*c;
+        delta3 = delta1-delta2;
+
+        System.out.println("O valor da equação é: "+delta3);
+    }
+
+}
