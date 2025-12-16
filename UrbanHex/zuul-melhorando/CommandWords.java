28da0852ed7ba4104639072ae9dadf4e2082d0c6
Marcos Lucas de Souza Machado <118454658+F1reZap@users.noreply.github.com>
2025-12-15 21:31:16 -0300

    Fix command order in validCommands array

--- UrbanHex/zuul-melhorando/CommandWords.java
+++ UrbanHex/zuul-melhorando/CommandWords.java
@@ -14,7 +14,7 @@ public class CommandWords
     // Adiciona as novas direções dos meus comandos divinos muahahahahahahhahahahahahahhahahahahahahahahahhahahaha
     //não funcinou então voltei pros padrões >:( jogo chatão
     private static final String[] validCommands = {
-        "go", "quit", "look", "help"
+        "go", "quit", "help", "look"
     };
 
     public CommandWords()
