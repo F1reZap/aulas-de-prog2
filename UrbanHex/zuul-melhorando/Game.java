5c928075e4d96e57bcc2051387b86d26990fa74a
Marcos Lucas de Souza Machado <118454658+F1reZap@users.noreply.github.com>
2025-12-15 21:11:22 -0300

    Add 'look' command to available command words

--- UrbanHex/zuul-melhorando/Game.java
+++ UrbanHex/zuul-melhorando/Game.java
@@ -116,7 +116,7 @@ public class Game
         System.out.println("around at the university.");
         System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        System.out.println("   go quit look help");
     }
 
     private void goRoom(Command command) 
